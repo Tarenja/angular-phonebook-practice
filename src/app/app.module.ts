2d084f6aaa2ea4afea0f7c118c96a5895e534cb6
Tarenja <tarenya@hotmail.com>
2021-03-04 16:57:10 +0100

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -10,6 +11,26 @@ import { ContactDetailsComponent } from './components/contact-details/contact-de
 import { FullNamePipe } from './pipes/full-name.pipe';
 import { AgePipe } from './pipes/age.pipe';
 import { ContactListComponent } from './components/contact-list/contact-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full', //only if entire path is empty string
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'contact-list',
+    component: ContactListComponent,
+  },
+  {
+    path: 'contact-details/:id',
+    component: ContactDetailsComponent,
+  },
+];
 
 @NgModule({
   declarations: [
@@ -22,7 +43,7 @@ import { ContactListComponent } from './components/contact-list/contact-list.com
     AgePipe,
     ContactListComponent,
   ],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/contact-details/contact-details.component.ts
+++ src/app/components/contact-details/contact-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { PhonebookService } from 'src/app/services/phonebook.service';
 import { Contact } from '../../model/contact';
 
@@ -10,15 +11,20 @@ import { Contact } from '../../model/contact';
 export class ContactDetailsComponent implements OnInit {
   contact: Contact = new Contact();
   // if dependencies are mentioned here, Angular will inject them for us
-  constructor(private service: PhonebookService) {}
+  constructor(
+    private service: PhonebookService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     // this is a way, but shouldn't do this, because PhonebookService might have all sorts of other dependencies
     // let service = new PhonebookService();
     // this.contact = service.getContactDetails(1);
     // do it like this:
-    this.service
-      .getContactDetails(1)
-      .subscribe((data) => (this.contact = data));
+    this.activatedRoute.params.subscribe((paramsData) => {
+      this.service
+        .getContactDetails(paramsData['id']) // :id variable in the routing in amm.module
+        .subscribe((data) => (this.contact = data));
+    });
   }
 }
